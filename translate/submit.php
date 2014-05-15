b8b9ae45687c8bda16c117ead6ce21c8d93d1e06
Ezekiel Elin <ezekielelin@me.com>
2014-05-15 17:13:59 -0400

    Stuff

--- translate/submit.php
+++ translate/submit.php
@@ -1,11 +1,14 @@
 <?php
 
 header('Location:/minecraft/tellraw?translated=true');
-$jobject = json_decode(file_get_contents('/home/ezfe/web-assets/translation-submits.json'),true);
-$newobject = array('language' => @$_GET['lang'], 'string' => @$_GET['string'], 'translate' => @$_GET['translate'], 'ip' => $_SERVER['REMOTE_ADDR']);
-array_push($jobject,$newobject);
-$send = json_encode($jobject);
-file_put_contents('/home/ezfe/web-assets/translation-submits.json',$send);
+
+if (@$_GET['string'] != "") {
+	$jobject = json_decode(file_get_contents('/home/ezfe/web-assets/translation-submits.json'),true);
+	$newobject = array('language' => @$_GET['lang'], 'string' => @$_GET['string'], 'translate' => @$_GET['translate'], 'ip' => $_SERVER['REMOTE_ADDR']);
+	array_push($jobject,$newobject);
+	$send = json_encode($jobject);
+	file_put_contents('/home/ezfe/web-assets/translation-submits.json',$send);	
+}
 
 echo '<a href="/minecraft/tellraw?translated=true">Click here</a>';
 ?>
