dc9c3f52fa249ce5d3660a7d6d6e9c82ef603e74
Ezekiel Elin <ezekielelin@me.com>
2014-08-18 15:01:45 -0400

    Started making alerts work better

--- js/tellraw.js
+++ js/tellraw.js
@@ -890,7 +890,7 @@ function refreshLanguage(dropdownSelection) {
 	if (lang[localStorage.getItem('langCode')] != undefined) {
 		$('*').refreshLanguage(localStorage.getItem('langCode'));
 	} else {
-		alert('error');
+		alert('An error occured trying to read your localStorage variable');
 	}
 	$('*').each(function(){
 		if ($(this).attr('version') != undefined && (localStorage['versionIndicators'] == true || localStorage['versionIndicators'] == undefined)) {
@@ -911,7 +911,6 @@ function refreshLanguage(dropdownSelection) {
 }
 
 function initialize() {
-	
 	if (localStorage.getItem('langCode') == undefined) {
 		if (lang[navigator.language.toLowerCase()] != undefined) {
 			localStorage.setItem('langCode',navigator.language.toLowerCase());
@@ -988,7 +987,8 @@ function initialize() {
 		}
 	});
 	$('#export').click(function(){
-		alert(JSON.stringify({"command":$('#command').val(),"jobject":jobject}))
+		$('.alerts').append('<div id="deleteConfirm" class="alert alert-info"><h4 lang="export.heading"></h4><p>' + JSON.stringify({"command":$('#command').val(),"jobject":jobject}) + '</p><p><button type="button" onclick="closeExport()" class="btn btn-default" lang="export.close"></button></p></div>');
+		refreshLanguage();
 	});
 	$('#translate_input').change(function(){
 		var val = translationStrings[$('#translate_input').val()];
