914fa0ee5fbc15b594d70548c67902526f39704d
Ezekiel Elin <ezekielelin@me.com>
2015-02-07 12:23:16 -0500

    Closed #106 Closed #105

--- js/tellraw.js
+++ js/tellraw.js
@@ -1111,7 +1111,7 @@ function jsonParse() {
 				if (/*templates[localStorage.getItem('jtemplate')].formatType == 'bookarray' || */templates[localStorage.getItem('jtemplate')].formatType == 'signset') {
 					$('#jsonPreview').append('<span id="jsonPreviewSpanElement'+ i +'"><hr></span>');
 				}
-			} else if (pageHash['index.' + i] == bookPage || !templates[localStorage.getItem('jtemplate')].formatType == 'bookarray') {
+			} else if (pageHash['index.' + i] == bookPage || templates[localStorage.getItem('jtemplate')].formatType != 'bookarray') {
 				var doClickEvent = false;
 				var doHoverEvent = false;
 				var popoverTitle = "";
