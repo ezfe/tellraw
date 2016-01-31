64b5be28347a9b8b984bcac64db089bac5a1402c
Ezekiel Elin <ezekielelin@me.com>
2016-01-31 16:59:26 -0500

    Revert "Increment version because wtf"
    This reverts commit 71e272234d8c082ce576abb329e0925b0e1e3f16.

--- js/tellraw.js
+++ js/tellraw.js
@@ -1,6 +1,6 @@
 var chars = [1,2,3,4,5,6,7,8,9,0,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var matchLength = 0;
-var version = 4;
+var version = 3;
 var tos_version = 1;
 var notice = {
 	"show": false,
