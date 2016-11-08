e9df5f025df01615645c0e037aa8d4c3f5397f41
Ezekiel Elin <ezekielelin@me.com>
2016-11-08 14:49:06 -0500

    Updated vote alert

--- js/tellraw.js
+++ js/tellraw.js
@@ -1557,8 +1557,8 @@ function initialize() {
 		lsm.removeItem('nextTimeAlert');
 	}
 
-	if (lsm.getItem('voteAlert') != "shown") {
-		lsm.setItem('voteAlert', "shown");
+	if (lsm.getItem('voteAlert2') != "shown") {
+		lsm.setItem('voteAlert2', "shown");
 		swal({"title":"You should vote","text":"Today is Election Day","cancelButtonText":"Close","confirmButtonText":"Where to vote?","showCancelButton":true,"closeOnConfirm":true},function(shouldLookup){if(shouldLookup){window.open("https://www.usa.gov/election-day#item-37327","_blank");}})
 	}
 
