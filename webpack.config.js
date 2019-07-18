aca907a4f393feaa6d28bd2ff138cfd277730a94
Ezekiel Elin <ezekielelin@me.com>
2019-07-18 19:18:04 -0400

    Tweaked build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
   entry: "./src/index.tsx",
@@ -20,7 +20,10 @@ module.exports = {
   module: {
     rules: [
       // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
+      },
       
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
