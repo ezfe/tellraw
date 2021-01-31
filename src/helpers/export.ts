b392ae10bafb770cc87ca45c52e642b04d8af493
Ezekiel Elin <ezekielelin@me.com>
2021-01-30 21:44:50 -0500

    Remove IDs from group children

--- src/helpers/export.ts
+++ src/helpers/export.ts
@@ -2,10 +2,14 @@ import { VERSION } from "../constants"
 import type { CommandType } from "../data/templates"
 import type { Snippet } from "../classes/Snippets/SnippetTypes/Snippet"
 import { duplicate_snippet } from "./duplicate_snippet"
+import { GroupSnippet } from "../classes/Snippets/SnippetTypes/GroupSnippet";
 
 function strip_id(snippet: Snippet): Snippet {
   delete snippet.id;
   snippet.hover_event_children = snippet.hover_event_children.map(child => strip_id(child));
+  if (snippet instanceof GroupSnippet) {
+    snippet.children = snippet.children.map(child => strip_id(child));
+  }
   return snippet;
 }
 
