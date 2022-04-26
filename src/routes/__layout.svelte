bf85ceb58909637013467784e44f8e973e1a65f2
Ezekiel Elin <ezekielelin@me.com>
2022-04-26 11:32:10 -0400

    Update dependencies and attempt to fix scrolling

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,12 @@
 <script>
 	import 'bootstrap/dist/css/bootstrap.min.css';
 	import '../app.css';
+	import { browser } from '$app/env';
+	if (browser) {
+		setTimeout(() => {
+			document.scrollingElement.scrollIntoView();
+		}, 100);
+	}
 </script>
 
 <svelte:head>
