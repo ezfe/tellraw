52c021e4b1af9922a1b0725a72579f18ee0b2c41
Ezekiel Elin <ezekielelin@me.com>
2025-01-10 21:42:08 -0500

    Remove download website link and workflow

--- src/lib/components/buttons/SiteActions.svelte
+++ src/lib/components/buttons/SiteActions.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
 	import { ButtonDropdown, DropdownItem, DropdownMenu, DropdownToggle } from '@sveltestrap/sveltestrap';
 	import CodeBranch from '../generic/Icons/CodeBranch.svelte';
-	import FileDownload from '../generic/Icons/FileDownload.svelte';
 	import List from '../generic/Icons/List.svelte';
-	import Twitter from '../generic/Icons/Twitter.svelte';
-	import WiFi from '../generic/Icons/WiFi.svelte';
 </script>
 
 <ButtonDropdown class="ms-2">
@@ -12,10 +9,6 @@
 		<List />
 	</DropdownToggle>
 	<DropdownMenu>
-		<DropdownItem href="https://github.com/ezfe/tellraw/releases/latest">
-			<FileDownload />
-			Download Website
-		</DropdownItem>
 		<DropdownItem href="https://legacy.minecraftjson.com">
 			<CodeBranch />
 			Legacy Version
